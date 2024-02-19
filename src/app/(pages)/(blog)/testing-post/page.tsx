caf22e5d5d6b530e8d36a24ec5cd752b5640dbad
Szymon Wojciechowski <kierej@gmail.com>
2024-02-19 11:02:04 +0100

    styling page of blog (max-width 660px)

--- src/app/(pages)/(blog)/testing-post/page.tsx
+++ src/app/(pages)/(blog)/testing-post/page.tsx
@@ -1,17 +1,16 @@
-import ThemeToggle from "@/components/blocks/theme-toggle";
-import {getRecursivelyBlocks} from "@/lib/notion/utils";
-import {RenderNotionBlocks} from "@/lib/notion/Render";
+import { getRecursivelyBlocks } from "@/lib/notion/utils";
+import { RenderNotionBlocks } from "@/lib/notion/Render";
 
 export default async function TestingPost() {
-    const notionRecursivelyBlock = await getRecursivelyBlocks(
-        "b3b3bc4cd37e4e75bec7984dcb4dd04c"
-    );
+  const notionRecursivelyBlock = await getRecursivelyBlocks(
+    "b3b3bc4cd37e4e75bec7984dcb4dd04c",
+  );
+
   return (
-    <div>
-      <ThemeToggle />
-        <div>
-            <RenderNotionBlocks blocks={notionRecursivelyBlock} />
-        </div>
-    </div>
+    <section className="flex justify-center">
+      <div className="max-w-[660px] mx-6 my-12">
+        <RenderNotionBlocks blocks={notionRecursivelyBlock} />
+      </div>
+    </section>
   );
 }
