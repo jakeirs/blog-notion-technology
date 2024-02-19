66382b8563708130b29d4b6973d1752ab15031ce
Szymon Wojciechowski <kierej@gmail.com>
2024-02-19 23:02:54 +0100

    added components page

--- src/app/(pages)/tech/page.tsx
+++ src/app/(pages)/tech/page.tsx
@@ -1,12 +1,9 @@
-import ThemeToggle from "@/components/blocks/theme-toggle";
-import { Button } from "@/components/ui/button";
-
-export default function MainPage() {
+export default function TechPage() {
   return (
     <section className="flex justify-center">
       <div className="max-w-[660px] mx-6 my-12">
         <span className="text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-orange-400">
-          This is heading
+          This is Tech Page
         </span>
       </div>
     </section>
