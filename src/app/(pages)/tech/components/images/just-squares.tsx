236dd354814d3d1d811486f4f28438a59e72d064
Szymon Wojciechowski <kierej@gmail.com>
2024-02-21 13:16:18 +0100

    few more patterns

--- src/app/(pages)/tech/components/images/just-squares.tsx
+++ src/app/(pages)/tech/components/images/just-squares.tsx
@@ -6,13 +6,16 @@ export const JustSquares = () => {
     <div>
       <div className="p-2 relative rounded-xl h-[200px] overflow-hidden">
         <div
-          className="absolute inset-0
+          className="absolute inset-0 bg-fixed
         {/*linear-pattern*/}
         {/*checkerboard*/}
-        {/*grid-pattern*/}
-        cross-pattern
+        grid-pattern
+        {/*cross-pattern*/}
         {/*zigzag-pattern*/}
         {/*waves-pattern*/}
+        {/*dots-pattern*/}
+        {/*radials-corner-pattern*/}
+
         "
         ></div>
         {/* any component. Component above you can treat as bg */}
