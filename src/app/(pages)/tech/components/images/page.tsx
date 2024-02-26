0b596b724e37c832efc2530525e1e3a31435d9e7
Szymon Wojciechowski <kierej@gmail.com>
2024-02-27 00:15:18 +0100

    add separator.tsx

--- src/app/(pages)/tech/components/images/page.tsx
+++ src/app/(pages)/tech/components/images/page.tsx
@@ -1,15 +1,19 @@
 import { PatternBg } from "@/components/ui/pattern-bg";
 import { ImageWithBg, src } from "@/components/ui/image-with-bg";
 import { ImageSimple } from "@/components/ui/image-simple";
+import { Separator } from "@/components/ui/separator";
 
 export default function ComponentsImagesPage() {
   return (
     <div>
-      <div className="flex flex-col" /* "flex flex-col" works */>
-        <ImageSimple src={src} />
-        <ImageWithBg src={src} ratio={16 / 9} />
-      </div>
+      <div className="grid grid-cols-3" /* "flex flex-col" works */>
+        <ImageWithBg src={src} ratio={16 / 9} vignette />
 
+        <ImageWithBg src={src} ratio={16 / 9} vignette />
+
+        <ImageWithBg src={src} ratio={16 / 9} vignette />
+      </div>
+      <Separator className="my-10" />
       <section className="grid-cols-[repeat(auto-fit,minmax(400px,auto))] grid gap-2">
         <ImageWithBg src={src} ratio={16 / 9} />
         <ImageWithBg src={src} ratio={16 / 9} />
