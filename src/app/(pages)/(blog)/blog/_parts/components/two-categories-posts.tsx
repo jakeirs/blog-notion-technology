6e126b88095801989f08f0fcaf5b55afab3cae90
Szymon Wojciechowski <kierej@gmail.com>
2024-02-28 18:43:34 +0100

    Added scaling image

--- src/app/(pages)/(blog)/blog/_parts/components/two-categories-posts.tsx
+++ src/app/(pages)/(blog)/blog/_parts/components/two-categories-posts.tsx
@@ -1,4 +1,4 @@
-import { ImageWithBg } from "@/components/ui/image-with-bg";
+import { ImageWithScaling } from "@/components/ui/image-with-scaling";
 
 const digitalSurvivalSrc = "/images/web-framing/digital_survival.webp";
 const howWeBuildSrc = "/images/web-framing/how_we_build.webp";
@@ -7,11 +7,11 @@ export const TwoCategoriesPosts = () => {
   return (
     <div className="grid grid-cols-2">
       <div className=".Block presentation .cards inner-shadow-fading">
-        <ImageWithBg src={digitalSurvivalSrc} ratio={16 / 9} vignette />
+        <ImageWithScaling src={digitalSurvivalSrc} ratio={16 / 9} vignette />
         <div className="LEading">Leading</div>
       </div>
       <div className=".Block presentation .cards">
-        <ImageWithBg src={howWeBuildSrc} ratio={16 / 9} vignette />
+        <ImageWithScaling src={howWeBuildSrc} ratio={16 / 9} vignette />
         <div className="LEading">Leading</div>
       </div>
     </div>
---
+++ src/components/ui/image-with-scaling.tsx
@@ -0,0 +1,41 @@
+import Image from "next/image";
+import { FunctionComponent } from "react";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+
+interface ImageWithSquaresProps {
+  src: string;
+  alt?: string;
+  ratio?: number;
+  vignette?: boolean;
+}
+
+export const src =
+  "https://images.unsplash.com/photo-1496692052106-d37cb66ab80c?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fGZyZWV8ZW58MHx8MHx8fDA%3D";
+
+export const ImageWithScaling: FunctionComponent<ImageWithSquaresProps> = ({
+  src = "",
+  alt = "Thumbnail",
+  ratio = 16 / 9,
+  vignette = false,
+}) => {
+  return (
+    <div className="group hover:scale-x-90 delay-150 ease-out transition duration-700 p-[5px] not-prose relative rounded-xl overflow-hidden">
+      <div className="grid-pattern inset-0 absolute" />
+      <AspectRatio
+        ratio={ratio}
+        className={"hover:scale-x-150 ease-out transition duration-700"}
+      >
+        {src ? (
+          <Image
+            src={src}
+            fill
+            sizes={"100vw"}
+            alt={alt}
+            className="object-cover h-full w-full max-w-full rounded-lg group-hover:scale-x-150 ease-out transition duration-700"
+          />
+        ) : null}
+      </AspectRatio>
+      {vignette && <div className="absolute inset-0 shadow-vignette" />}
+    </div>
+  );
+};
